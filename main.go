9e3cd2b2c80246fe35571cd7918c031cdd4ad815
Saadiq <43720122+snuweb@users.noreply.github.com>
2024-06-17 10:22:56 +0200

    todo

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/todos", getTodos).Methods("GET")
-	router.HandleFunc("/todos/{id}", getTodoByID).Methods("/GET")
-	router.HandleFunc("/todos", createTodo).Methods("POST")
-	router.HandleFunc("/todos{id}", updateTodo).Methods("PUT")
-	router.HandleFunc("/{id}", deleteTodo).Methods("DELETE")
-
-	http.ListenAndServe(":8080", router)
-
+	router := initRouter()
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- router.go
+++ router.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func initRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/todos", getTodos).Methods("GET")
+	router.HandleFunc("/todos/{id}", getTodoByID).Methods("GET")
+	router.HandleFunc("/todos", createTodo).Methods("POST")
+	router.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+	return router
+}
